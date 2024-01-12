087f0ee6b7e93b3c2c5b72950e2f0e5f75139e3d
Nara Park <skfk6352@naver.com>
2024-01-12 22:39:22 +0900

    docs: "프로젝트 설정"
    스프링 부트 프로젝트 초기 설정

---
+++ dev-together/src/main/java/com/project/devtogether/DevTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.project.devtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevTogetherApplication.class, args);
+    }
+
+}
