05d02d462890e3e2a4057449fbc85662e87a4faa
Nara Park <skfk6352@naver.com>
2024-01-15 03:10:01 +0900

    #7 feat: skill 엔티티 생성

---
+++ dev-together/src/main/java/com/project/devtogether/skill/domain/ProjectSkill.java
@@ -0,0 +1,31 @@
+package com.project.devtogether.skill.domain;
+
+import com.project.devtogether.project.domain.Project;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@Entity
+public class ProjectSkill {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id")
+    private Skill skill;
+}
