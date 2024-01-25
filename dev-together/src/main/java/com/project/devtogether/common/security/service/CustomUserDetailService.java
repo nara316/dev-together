7f0628f26a1a905e51843efad7a6ef368122b377
Nara Park <skfk6352@naver.com>
2024-01-26 03:40:02 +0900

    #13 feat: valid 애너테이션 관련 에러 메시지 정리

--- dev-together/src/main/java/com/project/devtogether/common/security/service/CustomUserDetailService.java
+++ dev-together/src/main/java/com/project/devtogether/common/security/service/CustomUserDetailService.java
@@ -1,5 +1,6 @@
 package com.project.devtogether.common.security.service;
 
+import com.project.devtogether.common.error.MemberErrorCode;
 import com.project.devtogether.common.exception.ApiException;
 import com.project.devtogether.common.security.user.CustomUserDetail;
 import com.project.devtogether.member.domain.Member;
@@ -24,6 +25,7 @@ public class CustomUserDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return memberRepository.findFirstByEmail(username)
                 .map(this::createUserDetails)
+                .orElseThrow(() -> new ApiException(MemberErrorCode.MEMBER_NOT_FOUND));
     }
 
     private CustomUserDetail createUserDetails(Member member) {
--- dev-together/src/main/java/com/project/devtogether/common/security/util/SecurityUtil.java
+++ dev-together/src/main/java/com/project/devtogether/common/security/util/SecurityUtil.java
@@ -1,5 +1,6 @@
 package com.project.devtogether.common.security.util;
 
+import com.project.devtogether.common.error.MemberErrorCode;
 import com.project.devtogether.common.exception.ApiException;
 import com.project.devtogether.common.security.user.CustomUserDetail;
 import org.springframework.security.core.Authentication;
@@ -10,6 +11,7 @@ public class SecurityUtil {
     public static String getCurrentUserEmail() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication.getName() == null) {
+            throw new ApiException(MemberErrorCode.MEMBER_NOT_FOUND);
         }
         return authentication.getName();
     }
@@ -17,6 +19,7 @@ public class SecurityUtil {
     public static Long getCurrentUserId() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication.getName() == null) {
+            throw new ApiException(MemberErrorCode.MEMBER_NOT_FOUND);
         }
         CustomUserDetail userDetails = (CustomUserDetail) authentication.getPrincipal();
         return userDetails.getId();
