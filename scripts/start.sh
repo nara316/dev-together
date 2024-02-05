cc84bfd34a7dc1e440b3b1a4277085045a588e3c
Nara Park <skfk6352@naver.com>
2024-02-06 08:18:44 +0900

    fix: 배포 쉘 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,16 @@
-PROJECT_ROOT="/home/ubuntu/dev-together"
-JAR_FILE="$PROJECT_ROOT/server-0.0.1-SNAPSHOT.jar"
+#!/bin/bash
+ CURRENT_PID=$(pgrep -f .jar)
+ echo "$CURRENT_PID"
+ if [ -z $CURRENT_PID ]; then
+         echo "no process"
+ else
+         echo "kill $CURRENT_PID"
+         kill -9 $CURRENT_PID
+         sleep 3
+ fi
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-
-TIME_NOW=$(date +%c)
-
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-source ~/.bashrc
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+ JAR_PATH="/home/ubuntu/dev-together/*.jar"
+ echo "jar path : $JAR_PATH"
+ chmod +x $JAR_PATH
+ nohup java -jar $JAR_PATH >> /home/ubuntu/dev-together/deploy.log 2>> /home/ubuntu/dev-together/deploy_err.log &
+ echo "jar fild deploy success"
