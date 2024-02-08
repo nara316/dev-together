a93fa6c28b5fdda4cc919479c8e9029f78900b0d
Nara Park <skfk6352@naver.com>
2024-02-08 22:28:26 +0900

    nginx 오류 수정

--- src/main/java/com/project/devtogether/common/security/SecurityConfig.java
+++ src/main/java/com/project/devtogether/common/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .requestMatchers("/swagger-ui.html").permitAll()
                 .requestMatchers("/swagger-ui/**").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
